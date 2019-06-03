e3e50cc51c79fcc7d9d558791f67ecb1eee763e6
Alican Dedekarginoglu <alicankargin@gmail.com>
2019-06-03 23:50:44 +0200

    Refactor ChatController to use MessageService

--- server/simplechat/src/main/java/com/alicankargin/simplechat/chat/controller/ChatController.java
+++ server/simplechat/src/main/java/com/alicankargin/simplechat/chat/controller/ChatController.java
@@ -1,18 +1,29 @@
 package com.alicankargin.simplechat.chat.controller;
 
 import com.alicankargin.simplechat.chat.business.model.MessageModel;
+import com.alicankargin.simplechat.message.business.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
 
 @Controller
 public class ChatController {
+    @Autowired
+    MessageService service;
 
     @MessageMapping("/sendMessage")
     @SendTo("/chat/public")
     public MessageModel sendMessage(MessageModel message) {
-        return message;
+        MessageModel model = service.saveMessage(message);
+        return model;
     }
 
+    @MessageMapping("/getMessages")
+    @SendTo("/chat/public")
+    public List<MessageModel> getMessages() {
+        return service.findAllMessages();
+    }
 }
